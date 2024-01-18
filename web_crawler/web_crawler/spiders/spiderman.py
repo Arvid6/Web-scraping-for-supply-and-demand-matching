faa7653d91ad6bc3cf4b27c42d0dafc91b3f27d8
Arvid <31167802+Arvid6@users.noreply.github.com>
2024-01-18 12:40:58 +0100

    changed file name

--- web_crawler/web_crawler/spiders/spiderman.py
+++ web_crawler/web_crawler/spiders/spiderman.py
@@ -3,14 +3,15 @@ from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 
 
-class PostnordSpider(CrawlSpider):
-    name = "postnord"
+class SpidermanSpider(CrawlSpider):
+    name = "spiderman"
     allowed_domains = ["transportfirma.se", "merchsweden.se"]
     start_urls = ["https://www.transportfirma.se/", "https://merchsweden.se/"]
 
     rules = (
         Rule(LinkExtractor(), callback='parse_item', follow=True),
     )
+
     def parse_item(self, response):
         self.links.append(response.url)
         for href in response.css('a::attr(href)'):
