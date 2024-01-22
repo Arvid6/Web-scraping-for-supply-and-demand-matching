1d7138287670a2be18c3b1844da288c4c09b1189
Lynxfor <102663128+Gabriel-Sundblad@users.noreply.github.com>
2024-01-22 14:18:42 +0100

    Sparande av Json
    Sparande av json fil utan att ersätta tidigare data

--- web_crawler/web_crawler/spiders/spindel_jens-anders.py
+++ web_crawler/web_crawler/spiders/spindel_jens-anders.py
@@ -18,16 +18,42 @@ for script in soppa(["script", "style"]):
 JensAndersjuveler = soppa.get_text()
 
 #Delar upp allt
-lines = (line.strip() for line in JensAndersjuveler.splitlines())
+rader = (line.strip() for line in JensAndersjuveler.splitlines())
 
 #Delar upp headlines till egna rader
-chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+bitar = (phrase.strip() for line in rader for phrase in line.split("  "))
 
 #Tar bort onödiga blank spaces
-JensAndersjuveler = '\n'.join(chunk for chunk in chunks if chunk)
+JensAndersjuveler = '\n'.join(chunk for chunk in bitar if chunk)
 
+#print(html)
 print(JensAndersjuveler)
 
-#Spara i Json fil
-with open("SlutetgottAlltgott.json", "w") as outfile:
-    json.dump(JensAndersjuveler, outfile)
+#Slår ihop URL med innehåll
+momentan_data = url + "     " + JensAndersjuveler
+
+#Kollar om json filen är tom
+try:
+    with open('SlutetgottAlltgott.json', 'r') as f:
+        data = json.load(f)
+    # If the file is not empty and contains valid JSON, data will contain that JSON
+    tom = 0
+except json.JSONDecodeError:
+    # If the file is empty or not valid JSON, a JSONDecodeError will be raised
+    tom = 1
+
+if tom == 1:
+    #Sparar det till SlutetgottAlltgott.json
+    with open("SlutetgottAlltgott.json", "w") as outfile:
+        json.dump(momentan_data, outfile)
+else:
+    #Laddar Slutetgottaltinggott.json in i kolonisten
+    with open('SlutetgottAlltgott.json', 'r') as f:
+        kolonisten = json.load(f)
+
+    #Slår ihop nya datan med gamla datan
+    Slutet = kolonisten + "     " + momentan_data
+
+    #Sparar det till SlutetgottAlltgott.json
+    with open("SlutetgottAlltgott.json", "w") as outfile:
+        json.dump(Slutet, outfile)
