075dca878b44f44c4bf88304c1b8d36b3b1f6112
EricVikberg <erivik-8@student.ltu.se>
2024-01-31 13:29:04 +0100

    SCB API
    Fixat dynamisk absolute path till certifikat-filerna, så nu borde det funka utan att skriva in sin egna path.
    
    Format på json-filen.

--- web_crawler/nace.py
+++ web_crawler/nace.py
@@ -1,45 +1,40 @@
 import requests
 import json
+import os
 
+# url till APIn
 url = "https://privateapi.scb.se/nv0101/v1/sokpavar/api/Je/HamtaForetag"
 
-# Path till cert-filar. Måste hitta nåt universiellt på det här, men byt till din egna path medan.
-cert_file_path = "C:\\Users\\Justf\\Desktop\\Scrape\\D0020E\\web_crawler\\test_cert.crt"
-private_key_path = "C:\\Users\\Justf\\Desktop\\Scrape\\D0020E\\web_crawler\\test_key3.pem"
-
+# Path till certifikatet och nyckeln
+cert_file_path = os.path.abspath("test_cert.crt")
+private_key_path = os.path.abspath("test_key3.pem")
 
 headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-data_test = {"Arbetsställestatus":"1",
-             "Variabler":[{"Varde1":"Luleå","Operator":"ArLikaMed","Variabel":"Postort"}],
-             "Kategorier":[{"Kategori":"Bransch","Kod": ["01110","as"], "BranschNiva":"3"}]}
+data_test = {"Arbetsställestatus": "1",
+             "Variabler": [{"Varde1": "Luleå", "Operator": "ArLikaMed", "Variabel": "Postort"}],
+             "Kategorier": [{"Kategori": "Bransch", "Kod": ["01110", "as"], "BranschNiva": "3"}]}
 
+response = requests.post(url, cert=(cert_file_path, private_key_path,), json=data_test, headers=headers)
+# response = requests.get(url, cert=(cert_file_path, private_key_path,), headers=headers)
 
-
-response = requests.post(url, cert=(cert_file_path, private_key_path,),json=data_test, headers=headers)
-#response = requests.get(url, cert=(cert_file_path, private_key_path,), headers=headers)
-
-#print(f"Response: {response.json()}")
+# print(f"Response: {response.json()}")
 
 test = response.content
 
 filtrera = response.json()
-print(test)
-
-namn = filtrera[0]['Företagsnamn']
-print(namn)
-
+print(filtrera)
 
 with open('cnt.json', 'wb') as f:
     f.write(test)
 
+fn = []
 
-foretagsnamn = ([d['Företagsnamn'] for d in filtrera])
+for d in filtrera:
+    foretagsnamn = d.get('Företagsnamn')
+    postort = d.get('PostOrt')
+    fn.extend([foretagsnamn, postort])
 
-fn_json = json.dumps(foretagsnamn)
+fn_json = json.dumps(fn, indent=1)
 
 with open("sample.json", "w") as outfile:
     outfile.write(fn_json)
-
-
-
-
