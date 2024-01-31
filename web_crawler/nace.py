ab9375f024e836d914a559426902647ae173a72c
EricVikberg <erivik-8@student.ltu.se>
2024-01-31 17:00:03 +0100

    SCB API
    Bara gjort om till engelska

--- web_crawler/nace.py
+++ web_crawler/nace.py
@@ -2,39 +2,43 @@ import requests
 import json
 import os
 
-# url till APIn
+# API url
 url = "https://privateapi.scb.se/nv0101/v1/sokpavar/api/Je/HamtaForetag"
 
-# Path till certifikatet och nyckeln
+# Path to certificate and private key.
 cert_file_path = os.path.abspath("test_cert.crt")
 private_key_path = os.path.abspath("test_key3.pem")
 
+# Arguments to be sent to the API
+search_city = "Stockholm"
+search_nace = "10840"
 headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
 data_test = {"Arbetsställestatus": "1",
-             "Variabler": [{"Varde1": "Luleå", "Operator": "ArLikaMed", "Variabel": "Postort"}],
-             "Kategorier": [{"Kategori": "Bransch", "Kod": ["01110", "as"], "BranschNiva": "3"}]}
+             "Variabler": [{"Varde1": search_city, "Operator": "ArLikaMed", "Variabel": "Postort"}],
+             "Kategorier": [{"Kategori": "Bransch", "Kod": [search_nace, search_nace], "BranschNiva": "3"}]}
 
+# Call to the API
 response = requests.post(url, cert=(cert_file_path, private_key_path,), json=data_test, headers=headers)
 # response = requests.get(url, cert=(cert_file_path, private_key_path,), headers=headers)
 
-# print(f"Response: {response.json()}")
 
-test = response.content
-
-filtrera = response.json()
-print(filtrera)
+# Response from API saved as a JSON-file
+content = response.content
 
 with open('cnt.json', 'wb') as f:
-    f.write(test)
+    f.write(content)
 
-fn = []
+# Response from API filtered on Company name and City
+output = response.json()
+print(output)
+temp_list = []
 
-for d in filtrera:
-    foretagsnamn = d.get('Företagsnamn')
-    postort = d.get('PostOrt')
-    fn.extend([foretagsnamn, postort])
+for d in output:
+    company_name = d.get('Företagsnamn')
+    city = d.get('PostOrt')
+    temp_list.extend([company_name, city])
 
-fn_json = json.dumps(fn, indent=1)
+filtered_output = json.dumps(temp_list, indent=1)
 
-with open("sample.json", "w") as outfile:
-    outfile.write(fn_json)
+with open("found_companies.json", "w") as outfile:
+    outfile.write(filtered_output)
