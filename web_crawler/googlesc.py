fd20aaf16cf3308019fe82b545ea3d5e7f06f5cd
Arvid <31167802+Arvid6@users.noreply.github.com>
2024-02-01 10:22:55 +0100

    Removed language barrier

--- web_crawler/googlesc.py
+++ web_crawler/googlesc.py
@@ -19,8 +19,6 @@ def getSeach():
         params = {
           "q": word,
           "location": loc, # OPTIONAL
-          "hl": "sv", # OPTIONAL
-          "gl": "se", # OPTIONAL
           "google_domain": "google.se",
           "api_key": "5f4693b65a0e9d1a165d3581f1f4ab4dd2dab0a7a77248a0d09da6fa33d01821"
         }
