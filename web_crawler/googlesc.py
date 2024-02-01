2c927503409dfdd701c8f011523f971fe79dd30d
Arvid <31167802+Arvid6@users.noreply.github.com>
2024-02-01 10:16:30 +0100

    google bot

---
+++ web_crawler/googlesc.py
@@ -0,0 +1,30 @@
+from serpapi import GoogleSearch
+
+def setSearch(txt):
+    global swag
+    swag = txt
+
+
+swag = ["Pizza", "Burger", "Pasta"]
+
+
+for word in swag:
+    params = {
+      "q": word,
+      "location": "Lulea, Norrbotten County, Sweden", # OPTIONAL
+      "hl": "sv", # OPTIONAL
+      "gl": "se", # OPTIONAL
+      "google_domain": "google.se",
+      "api_key": "5f4693b65a0e9d1a165d3581f1f4ab4dd2dab0a7a77248a0d09da6fa33d01821"
+    }
+
+    search = GoogleSearch(params)
+    results = search.get_dict()
+
+    organic_results = results.get('organic_results', [])
+
+    # NUMBER OF RESULTS
+    urls = [result.get('link') for result in organic_results[:4]]
+
+    print(f"Search word: {word} ")
+    print(urls)
